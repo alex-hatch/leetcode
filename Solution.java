380014018f9536dc5220710fe95c33d638061233
Alex Hatch <89677719+alex-hatch@users.noreply.github.com>
2022-02-09 23:02:24 -0800

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        HashMap<Integer, Integer> sumCount = new HashMap<>();
+        sumCount.put(0,1);
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if(sumCount.containsKey(sum - k)) {
+                count += sumCount.get(sum - k);
+            }
+            sumCount.put(sum, sumCount.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+}
